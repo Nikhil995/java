e212087780f9d0f684fc93c8ea1d7a22a092175f
Nikhil agrawal <99258446+Nikhil995@users.noreply.github.com>
2022-07-21 15:07:40 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+public class greater {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ennter a no");
+        int a= sc.nextInt();
+        System.out.println(a>10);
+
+    }
+}
