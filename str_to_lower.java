7346cca6ff4ea37ec7cbf526d93a2ed097e2cf49
Nikhil agrawal <99258446+Nikhil995@users.noreply.github.com>
2022-07-21 15:08:53 +0530

    Add files via upload

---
+++ str_to_lower.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class str_to_lower {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a string");
+        String a=sc.nextLine();
+        System.out.println(a.toLowerCase());
+    }
+}
