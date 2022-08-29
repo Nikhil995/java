7b18e16122dce343e5c50d7fc144327400679e2e
Nikhil agrawal <99258446+Nikhil995@users.noreply.github.com>
2022-08-29 21:36:46 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,12 @@
+class simple
+{
+	public static void main(String args[])
+	{
+		int p,r,t,s;
+		p=Integer.parseInt(args[0]);
+		r=Integer.parseInt(args[1]);
+		t=Integer.parseInt(args[2]);
+		s=((p*r*t)/100);
+		System.out.println(s);
+	}
+}
